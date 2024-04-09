76d4deb82ace996a1eaaa581b2ae18a9c82f880d
Madlen <154542118+madlen77@users.noreply.github.com>
2024-04-09 15:26:15 +0200

    worked on basic tailwind theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import logo from "./logo.svg";
+import woman from "./woman.svg";
 import "./App.css";
-
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="text-3xl font-bold text-dark">Hi</h1>
+        <img src={woman} alt="woman is stretching" />
         <p>
           <button
             type="button"
